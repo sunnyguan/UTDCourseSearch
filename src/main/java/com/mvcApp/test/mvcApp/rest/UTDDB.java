e217b113a825f22f0b165e2d67c42b2eecdcb06a
Sunny Guan <sunny.peng.guan@gmail.com>
2020-09-04 10:12:56 -0500

    changed ngrok link

--- src/main/java/com/mvcApp/test/mvcApp/rest/UTDDB.java
+++ src/main/java/com/mvcApp/test/mvcApp/rest/UTDDB.java
@@ -372,7 +372,7 @@ public class UTDDB {
 
       Unirest.setTimeouts(0, 0);
       HttpResponse<String> response =
-          Unirest.get("http://0a47493587cb.ngrok.io/api/coursetest?query=" + java.net.URLEncoder.encode(searchQuery))
+          Unirest.get("http://6dc75fd0dfc2.ngrok.io/api/coursetest?query=" + java.net.URLEncoder.encode(searchQuery))
               .header("Content-Type", "application/json")
               .asString();
       String resp = response.getBody(); // .substring(21);
